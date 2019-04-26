19b4e56601f294326b87ecdfe99bd30e1b38a093
ram <ramkrishan@kontikilabs.com>
2019-04-26 18:17:11 +0530

    Updates

---
+++ precache-manifest.506fca19ee71ce19b64a7e1705650ac9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0844b197c4f18213fc3a97e703f1acd8",
+    "url": "/text-delimiter/static/media/settings.0844b197.svg"
+  },
+  {
+    "revision": "9e76c55dd28c66c5d159",
+    "url": "/text-delimiter/static/js/runtime~main.51a06460.js"
+  },
+  {
+    "revision": "b588dd1b57fc31f45df0",
+    "url": "/text-delimiter/static/js/main.a7b6d17c.chunk.js"
+  },
+  {
+    "revision": "d601fd6f9fe46eaf640d",
+    "url": "/text-delimiter/static/js/2.408f6dd2.chunk.js"
+  },
+  {
+    "revision": "b588dd1b57fc31f45df0",
+    "url": "/text-delimiter/static/css/main.430dda46.chunk.css"
+  },
+  {
+    "revision": "d601fd6f9fe46eaf640d",
+    "url": "/text-delimiter/static/css/2.ed66abb8.chunk.css"
+  },
+  {
+    "revision": "34f568ed4c9c4df415ce3648769214ae",
+    "url": "/text-delimiter/index.html"
+  }
+];
